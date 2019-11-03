9fdfdd942338a68a60982143e0b3483d92dd7a9d
johan suarez <jhoanseb1992@gmail.com>
2019-11-03 21:56:41 +1100

    add dynamic templates and contact model for ndb

---
+++ contacts_project_web/contact_model.py
@@ -0,0 +1,7 @@
+from google.cloud import ndb
+
+
+class Contact(ndb.Model):
+    name = ndb.StringProperty()
+    phone = ndb.StringProperty()
+    email = ndb.StringProperty()
--- contacts_project_web/main.py
+++ contacts_project_web/main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from contact_model import Contact
+from google.cloud import ndb
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route(r'/', methods=['GET'])
 def hello_world():
-	return 'Hello, world'
+	return render_template('contact_book.html')
+
+
+@app.route(r'/add', methods=['GET', 'POST'])
+def add_contact():
+	client = ndb.Client()
+	if request.form:
+		with client.context():
+			contact = Contact(
+				name=request.form.get('name'),
+				phone=request.form.get('phone'),
+				email=request.form.get('email')
+			)
+			contact.put()
+
+	return render_template('add_contact.html')
 
 
 if __name__ == '__main__':
