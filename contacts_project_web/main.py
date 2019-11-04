56c5f15d2090a370a67377b5b73e6a053e65fb70
johan suarez <jhoanseb1992@gmail.com>
2019-11-04 17:13:41 +1100

    first working version

--- contacts_project_web/main.py
+++ contacts_project_web/main.py
@@ -5,17 +5,19 @@ from google.cloud import ndb
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 app.debug = True
+client = ndb.Client()
 
 
 @app.route(r'/', methods=['GET'])
 def hello_world():
-    contacts = Contact.query().fetch()
+    with client.context():
+        contacts = Contact.query().fetch()
     return render_template('contact_book.html',contacts=contacts)
 
 
 @app.route(r'/add', methods=['GET', 'POST'])
 def add_contact():
-    client = ndb.Client()
+
     if request.form:
         with client.context():
             contact = Contact(
@@ -26,12 +28,13 @@ def add_contact():
             contact.put()
             flash('Contact Add it!!')
 
-        return render_template('add_contact.html')
+    return render_template('add_contact.html')
 
 
 @app.route(r'/contacts/<uid>', methods=['GET'])
 def contact_details(uid):
-    contact = Contact.get_by_id(int(uid))
+    with client.context():
+        contact = Contact.get_by_id(int(uid))
 
     if not contact:
         return redirect('/', code=301)
@@ -41,9 +44,11 @@ def contact_details(uid):
 
 @app.route(r'/delete', methods=['POST'])
 def delete_contact():
-    contact = Contact.get_by_id(int(request.form.get('uid')))
-    contact.key.delete()
-    return redirect('/contacts/{}'.format(contact.key.id()))
+    with client.context():
+        contact = Contact.get_by_id(int(request.form.get('uid')))
+        contact.key.delete()
+        red_link = '/contacts/{}'.format(contact.key.id())
+    return redirect(red_link)
 
 
 if __name__ == '__main__':
